34203a426fe273a56d9a2d3de4ec8420a9848195
Aditya Joshi <5adityajoshi@gmail.com>
2026-04-22 19:33:40 +0530

    Add basic calculator
    Implement a basic calculator with add, subtract, multiply, and divide functions.

---
+++ BasicCalculator.py
@@ -0,0 +1,50 @@
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1,n2):
+    return n1-n2
+def multiply(n1,n2):
+    return n1*n2
+def divide(n1,n2):
+    return n1/n2
+
+operations={
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":divide,
+}
+
+while True:
+    print(logo)
+    print("Welcome to the Python Calculator!")
+    number_1=int(input("Enter the first number: "))
+    should_continue=True
+    while should_continue:
+        operator=input("Choose an operation from '+','-','*','/': ")
+        number_2=int(input("Enter the second number: "))
+        result=operations[operator](number_1,number_2)
+        print(f"The answer is: {result}")
+        should_continue=input("Do you want to continue with the result of the previous operation? Type 'yes' or 'no': ").lower()
+        if should_continue=='yes':
+            number_1=result
+        else:
+            should_continue=False
+            print("\n"*50)
